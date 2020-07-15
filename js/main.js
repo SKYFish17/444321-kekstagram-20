f8d47d704569767b80550fee4a4a79c71462f34f
SKYFish17 <nwcorp93@yandex.ru>
2020-07-15 15:11:45 +0300

    создаёт, подключает и использует модуль наложения эффектов на изображение

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ var imgUploadOverlay = imgUploadContainer.querySelector('.img-upload__overlay');
 var imgUploadCancel = imgUploadContainer.querySelector('.img-upload__cancel');
 var hashtagsInput = imgUploadContainer.querySelector('.text__hashtags');
 var commentInput = imgUploadContainer.querySelector('.text__description');
+var effectsList = imgUploadOverlay.querySelector('.effects__list');
+var effectsPreviewNone = effectsList.querySelector('#effect-none');
+var effectSlider = imgUploadOverlay.querySelector('.effect-level');
+var effectLevelContainer = imgUploadOverlay.querySelector('.effect-level');
+var effectLevelPin = effectLevelContainer.querySelector('.effect-level__pin');
 
 var scaleContainer = imgUploadContainer.querySelector('.scale');
 var scaleSmaller = scaleContainer.querySelector('.scale__control--smaller');
@@ -26,9 +31,9 @@ var openUploadOverlay = function () {
 
   scaleSmaller.addEventListener('click', window.formScale.onScaleSmallerClick);
   scaleSmaller.addEventListener('keydown', window.formScale.onScaleSmallerPressEnter);
-  effectsList.addEventListener('change', onEffectsItemClick, true);
+  effectsList.addEventListener('change', window.formEffects.onEffectsItemClick, true);
 
-  effectLevelPin.addEventListener('mouseup', onEffectLevelPinMouseup);
+  effectLevelPin.addEventListener('mouseup', window.formEffects.onEffectLevelPinMouseup);
 
   if (effectsPreviewNone.checked) {
     effectSlider.classList.add('hidden');
@@ -49,9 +54,9 @@ var closeUploadOverlay = function () {
 
   scaleSmaller.removeEventListener('click', window.formScale.onScaleSmallerClick);
   scaleSmaller.removeEventListener('keydown', window.formScale.onScaleSmallerPressEnter);
-  effectsList.removeEventListener('change', onEffectsItemClick, true);
+  effectsList.removeEventListener('change', window.formEffects.onEffectsItemClick, true);
 
-  effectLevelPin.removeEventListener('mouseup', onEffectLevelPinMouseup);
+  effectLevelPin.removeEventListener('mouseup', window.formEffects.onEffectLevelPinMouseup);
 };
 
 var onUploadOverlayEscPress = function (evt) {
@@ -68,106 +73,6 @@ imgUploadInput.addEventListener('change', function () {
 imgUploadCancel.addEventListener('click', function () {
   closeUploadOverlay();
 });
-
-//  масштабирование изображения
-var imgUploadPreviewContainer = imgUploadOverlay.querySelector('.img-upload__preview');
-var imgUploadPreview = imgUploadPreviewContainer.querySelector('img');
-//  Наложение эффекта на изображение
-var effectsList = imgUploadOverlay.querySelector('.effects__list');
-var effectSlider = imgUploadOverlay.querySelector('.effect-level');
-var effectsPreviewNone = effectsList.querySelector('#effect-none');
-var previousEffectName = '';
-
-var onEffectsItemClick = function (evt) {
-  var effectLabel = evt.target.nextElementSibling.querySelector('.effects__preview');
-  var effectName = effectLabel.classList[1];
-
-  if (previousEffectName) {
-    imgUploadPreview.classList.remove(previousEffectName);
-  }
-
-  if (effectName === 'effects__preview--none') {
-    effectSlider.classList.add('hidden');
-  } else {
-    effectSlider.classList.remove('hidden');
-  }
-
-  previousEffectName = effectName;
-  imgUploadPreview.classList.add(effectName);
-  changeEffectLevel(window.constants.MAX_PIN_POSITION);
-};
-
-// фильтры
-var effectLevelContainer = imgUploadOverlay.querySelector('.effect-level');
-var effectLevelPin = effectLevelContainer.querySelector('.effect-level__pin');
-var effectLevelDepth = effectLevelContainer.querySelector('.effect-level__depth');
-var effectLevelValue = effectLevelContainer.querySelector('.effect-level__value');
-
-var testPinPosition = 150;//  убрать после полной реализации обработки соб. слайдера
-
-var getFilter = function (effectType, effectMinLevel, effectMaxLevel, unit, pinPosition) {
-  var effectLevel;
-  var filter;
-  var effectLevelDifference = effectMaxLevel - effectMinLevel;
-
-  effectLevel = effectMinLevel + pinPosition / window.constants.MAX_PIN_POSITION * effectLevelDifference;
-
-  if (unit !== 'none') {
-    filter = effectType + '(' + effectLevel + unit + ')';
-  } else {
-    filter = effectType + '(' + effectLevel + ')';
-  }
-
-  return filter;
-};
-
-var getRatio = function (numberOne, numberTwo, sign) {
-  var ratio;
-
-  if (sign) {
-    ratio = numberOne / numberTwo * window.constants.MAX_PERCENT + sign;
-  } else {
-    ratio = numberOne / numberTwo * window.constants.MAX_PERCENT;
-  }
-
-  return ratio;
-};
-
-var renderActualEffectLevel = function (pinPosition) {
-  effectLevelPin.style.left = getRatio(pinPosition, window.constants.MAX_PIN_POSITION, '%');
-  effectLevelDepth.style.width = getRatio(pinPosition, window.constants.MAX_PIN_POSITION, '%');
-  effectLevelValue.value = getRatio(pinPosition, window.constants.MAX_PIN_POSITION);
-};
-
-var changeEffectLevel = function (pinPosition) {
-
-  switch (imgUploadPreview.className) {
-    case 'effects__preview--none':
-      imgUploadPreview.style.filter = '';
-      break;
-    case 'effects__preview--chrome':
-      imgUploadPreview.style.filter = getFilter('grayscale', 0, 1, 'none', pinPosition);
-      break;
-    case 'effects__preview--sepia':
-      imgUploadPreview.style.filter = getFilter('sepia', 0, 1, 'none', pinPosition);
-      break;
-    case 'effects__preview--marvin':
-      imgUploadPreview.style.filter = getFilter('invert', 0, 100, '%', pinPosition);
-      break;
-    case 'effects__preview--phobos':
-      imgUploadPreview.style.filter = getFilter('blur', 0, 3, 'px', pinPosition);
-      break;
-    case 'effects__preview--heat':
-      imgUploadPreview.style.filter = getFilter('brightness', 1, 3, 'none', pinPosition);
-      break;
-  }
-
-  renderActualEffectLevel(pinPosition);
-};
-
-var onEffectLevelPinMouseup = function () {
-  changeEffectLevel(testPinPosition);
-};
 
 //  хэштеги
 var checksDuplicateTags = function (tags) {
